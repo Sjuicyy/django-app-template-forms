537d5b4a69add0651de37c8b58539c2215974692
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-13 22:12:45 +0545

    added context and manipulated

--- pages/views.py
+++ pages/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_view(request,*args,**kwargs):
-    my_context={
-        
-    }
     return render(request, "home.html",{})
 
-
 def about_view(request,*args,**kwargs):
-    return render(request, "about.html",{})
+    my_context = {
+        "my_name":"my name is sammy",
+        "my_age":55,
+        "my_list": [123,456,789,135,579],
+    }
+    return render(request, "about.html",my_context)
 
 
 def contact_view(request,*args,**kwargs):
