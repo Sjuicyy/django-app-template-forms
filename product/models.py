fa4b078043a8262d89a774cb1f83bcb30337ab8f
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-13 18:31:19 +0545

    added pages and dbsqlite change

--- product/models.py
+++ product/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Products(models.Model):
     name = models.CharField(max_length=50)
-    age  = models.FloatField()
+    price = models.FloatField()
     comment = models.TextField()
