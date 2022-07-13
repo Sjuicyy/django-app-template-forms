7c5008d53daaa7df53d351c0bf17511d09567ed5
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-13 19:10:01 +0545

    added 4 pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_view(*args,**kwargs):
+    return HttpResponse("<h1>this is home view<h1>")
+
+
+def about_view(*args,**kwargs):
+    return HttpResponse("<h1>this is about view<h1>")
+
+
+def contact_view(*args,**kwargs):
+    return HttpResponse("<h1>this is contact view<h1>")
+
+def address_view(*args,**kwargs):
+    return HttpResponse("<h1>this is address view<h1>")
--- project1/urls.py
+++ project1/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import home_view, about_view, contact_view, address_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about', about_view),
+    path('contact', contact_view),
+    path('address', address_view),
+   
 ]
