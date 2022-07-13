76fb2445134563211ccb851131f73ed68b875b6b
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-13 21:28:38 +0545

    html edited with block content

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_view(request,*args,**kwargs):
+    my_context={
+        
+    }
     return render(request, "home.html",{})
 
 
